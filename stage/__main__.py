d365ff193c122bfce75431d0bd104f3cc2e269b0
cSDes1gn <christian911@sympatico.ca>
2021-03-05 12:29:29 -0500

    testing new main config

--- stage/__main__.py
+++ stage/__main__.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import sys
 
-from stage.aws.sqs import SQSClient
+from stage.service.service import StageServer
 
-SQSClient().poll_sqs()
+if len(sys.argv) != 2:
+    sys.exit('Syntax: %s COMMAND' % sys.argv[0])
+
+cmd = sys.argv[1].lower()
+# initialize staging server
+service = StageServer(name='iris_staging_server', pid_dir='/tmp')
+
+if cmd == 'start':
+    service.start()
+elif cmd == 'stop':
+    service.stop()
+elif cmd == 'status':
+    if service.is_running():
+        print("Service is running.")
+    else:
+        print("Service is not running.")
+else:
+    sys.exit('Unknown command "%s".' % cmd)
