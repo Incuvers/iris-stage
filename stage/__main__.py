42af4bd677cb5ce0b8a73de500e960581c5c97e2
cSDes1gn <christian911@sympatico.ca>
2021-03-05 13:43:48 -0500

    add configurable service vars

--- stage/__main__.py
+++ stage/__main__.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import os
 import sys
+import logging.config
 
 from stage.service.service import StageServer
+
+SERVICE_NAME = os.environ['SERVICE_NAME']
+PID_DIR = os.environ['PID_DIR']
 
 if len(sys.argv) != 2:
     sys.exit('Syntax: %s COMMAND' % sys.argv[0])
 
 cmd = sys.argv[1].lower()
-# initialize staging server
-service = StageServer(name='iris_staging_server', pid_dir='/tmp')
+# instantiate staging server
+service = StageServer(name=SERVICE_NAME, pid_dir=PID_DIR)
 
 if cmd == 'start':
-    print("Starting service.")
+    print("Starting %s service in %s", SERVICE_NAME, PID_DIR)
     service.start()
 elif cmd == 'stop':
-    print("Stopping service.")
+    print("Halting %s service in %s", SERVICE_NAME, PID_DIR)
     service.stop()
 else:
     sys.exit('Unknown command "%s".' % cmd)
