590a0107bad4b1dbb49ba2ce5e949d1b80f07e60
cSDes1gn <christian911@sympatico.ca>
2021-03-05 12:29:48 -0500

    absolving sqs of pipelining facilitation

--- stage/aws/sqs.py
+++ stage/aws/sqs.py
@@ -13,12 +13,8 @@ This triggers the server to pull the .snap file from s3, remove the current
 snap and install the new snap file
 """
 
-
 import boto3
 import logging.config
-
-from stage.aws.s3 import S3Client
-from stage.snap.installer import Installer
 
 class SQSClient:
 
@@ -26,56 +22,51 @@ class SQSClient:
         self.logger = logging.getLogger(__name__)
         # Create SQS client
         self.sqs = boto3.client('sqs')
-        self.s3_client = S3Client()
-        self.installer = Installer()
-        self.logger.info("%s instantiated successfully.", __name__)
-
-    def poll_sqs(self) -> None:
-        """
-        Polls SQS server for snap push completion messages.
-        """
-        # List SQS queues
+        # Latch on available SQS queue
         response = self.sqs.list_queues()
         self.logger.info("SQS Queue list: %s", response['QueueUrls'])
-        sqs_queue_url=response['QueueUrls'][-1]
+        self.sqs_queue_url=response['QueueUrls'][-1]
+        self.logger.info("%s instantiated successfully.", __name__)
 
-        while True:
-            # Receive message from SQS queue
-            try:
-                response = self.sqs.receive_message(
-                    QueueUrl=sqs_queue_url,
-                    AttributeNames=[
-                        'SentTimestamp'
-                    ],
-                    MaxNumberOfMessages=1,
-                    MessageAttributeNames=[
-                        'All'
-                    ],
-                    VisibilityTimeout=0,
-                    WaitTimeSeconds=20
-                )
-            except BaseException as exc:
-                self.logger.exception(
-                    "An exception occured when making sqs request: %s", exc)
-                continue
-            
-            self.logger.info("Received Response: %s", response)
-            
-            try:
-                message = response['Messages'][0]
-            except KeyError:
-                pass
-            else:
-                self.logger.info("Receiving and digesting message")
-                # digest recieved message and launch a service
-                receipt_handle = message['ReceiptHandle']
-                # Delete received message from queue
-                self.sqs.delete_message(
-                    QueueUrl=sqs_queue_url,
-                    ReceiptHandle=receipt_handle
-                )
-                self.logger.info("Pulling recent snap from s3 bucket.")
-                self.s3_client.pull()
-                self.logger.info("Installing pulled snap.")
-                self.installer.install()
-                self.logger.info("Snap install complete.")
+    def poll_sqs(self) -> bool:
+        """
+        Polls SQS server for snap push completion messages.
+
+        :returns: True if SQS poll returns a notification and False otherwise
+        """
+        # Receive message from SQS queue
+        try:
+            response = self.sqs.receive_message(
+                QueueUrl=self.sqs_queue_url,
+                AttributeNames=[
+                    'SentTimestamp'
+                ],
+                MaxNumberOfMessages=1,
+                MessageAttributeNames=[
+                    'All'
+                ],
+                VisibilityTimeout=0,
+                WaitTimeSeconds=20
+            )
+        except BaseException as exc:
+            self.logger.exception(
+                "An exception occured when making sqs request: %s", exc)
+            return False
+        
+        self.logger.info("Received Response: %s", response)
+        
+        try:
+            message = response['Messages'][0]
+        except KeyError:
+            pass
+        else:
+            self.logger.info("Receiving and digesting message")
+            # digest recieved message and launch a service
+            receipt_handle = message['ReceiptHandle']
+            # Delete received message from queue
+            self.sqs.delete_message(
+                QueueUrl=self.sqs_queue_url,
+                ReceiptHandle=receipt_handle
+            )
+            return True
+        return False
